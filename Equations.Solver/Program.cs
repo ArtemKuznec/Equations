5849af7c7445fb4c7d83f4872a7e88dbce1ed804
a.kuznetsov <a.kuznetsov@mcad.ru>
2025-02-12 11:15:44 +0600

    Добавил консольный проект

---
+++ Equations.Solver/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Квадратное уравнение имеет вид: ax^2 + bx + c = 0, где a != 0");
+        Console.WriteLine("Прочесть данные с клавиатуры - 1");
+        Console.WriteLine("Прочесть данные из файла - 2");
+        Console.WriteLine("Выйти из приложения - 3");
+    }
+}
