5f96285c23676b03b48796158d2352d465835cf3
a.kuznetsov <a.kuznetsov@mcad.ru>
2025-02-12 13:27:45 +0600

    Убрал не использующиеся директивы

--- Equations.Solver/Utilities/MathUtilities.cs
+++ Equations.Solver/Utilities/MathUtilities.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Equations.Solver.Utilities
+﻿namespace Equations.Solver.Utilities
 {
     public static class MathUtilities
     {
